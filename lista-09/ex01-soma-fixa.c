734d3ce9596b0a5a319386c1ccb572128fcab38c
brunoconcli <bruno.concli@becomex.com.br>
2025-06-09 16:43:59 -0300

    refactor: Removidas mains e aprimorada modularizacao

--- lista-09/ex01-soma-fixa.c
+++ lista-09/ex01-soma-fixa.c
@@ -1,37 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "ex06-alocar.c"
 
 int** somarMatrizes(int a[3][3], int b[3][3])
 {
-	int** c = (int**) malloc(3*sizeof(int*));
-	if (c!=NULL)
-	{
-		for (int p = 0; p<2; p++)
-		{
-			c[p] = (int*) malloc(3 * sizeof(int));
-			if (c[p]==NULL) return(1);
-		}
-	}
+	int** c = alocarMatriz(3, 3);
 	for (int i = 0; i < 3; i++)
 	{
+		int valor;
 		for (int j = 0;  j<3; j++)
 		{
-			c[i][j] = a[i][j] + b[i][j];
+			valor = a[i][j] + b[i][j];
+			c[i][j] = valor;
 		}
 	}
 	return c;
-}
-
-int main()
-{
-	int a[3][3] =
-	{
-		{1,3,5},{7,9,11},{13,15,17}
-	};
-	int b[3][3] =
-	{
-		{2,4,6},{8,10,12},{14,16,18}
-	};
-
-	// continuar!! pulando para o exercicio 06
 }
--- lista-09/ex01-soma.c
+++ lista-09/ex01-soma.c
@@ -1,32 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include "ex06-alocar.c"
 
-int somarMatrizes(int** mtz01, int** mtz02, int linhas, int colunas)
+int** somarDinamicamente(int** a, int** b, int linhas, int colunas)
 {
-	int mtzResultado[linhas][colunas];
-	for (int i < 0; i < linhas; i++)
+	if (linhas != colunas) return NULL;
+	int** c = alocarMatriz(linhas, colunas);
+	for (int i = 0; i<linhas; i++)
 	{
-		for (int j = 0; j < colunas; j++)
+		for (int j = 0; j<colunas; j++)
 		{
-			mtzResultado[i][j] = mtz01[i][j] + mtz02[i][j];
+			int valor = a[i][j] + b[i][j];
+			c[i][j] = valor;
 		}
 	}
-	return mtzResultado;
-}
-
-int main()
-{
-	int mtz01[3][3] =
-	{
-		{1, 3, 5},
-		{7, 9, 11},
-		{13, 15, 17}
-	};
-	int mtz02[3][3] =
-	{
-		{2, 4, 6},
-		{8, 10, 12},
-		{14, 16, 18}
-	};
-	// a completar!!!
+	return c;
 }
