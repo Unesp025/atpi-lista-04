e3edbcf73e320f03e0a56926f166f76ef1de3940
brunoconcli <bruno.concli@becomex.com.br>
2025-06-09 17:45:20 -0300

    feat: Adicionada main para testar 'pertence'

---
+++ lista-09/main-pertence.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include"ex03-pertence.c"
+#include "ex07-liberar.c"
+#include "ex08-alocar-input.c"
+#include "ex09-imprimir.c"
+
+int main ()
+{
+    int linhas, colunas;
+    printf("Digite a quantidade de linhas: ");
+    scanf("%d", &linhas);
+    printf("Digite a quantidade de colunas: ");
+    scanf("%d", &colunas);
+    
+    int** a = preencherMatrizDinamica(linhas, colunas);
+    
+    int busca;
+    printf("\nQual elemento deseja procurar?: ");
+    scanf("%d", &busca);
+
+    printf("\nMatriz de busca:\n");
+    imprimirMatriz(a, linhas, colunas);
+    imprimirIndice(busca, a, linhas, colunas);
+
+    liberarEspaco(a, linhas);
+    return 0;
+}
